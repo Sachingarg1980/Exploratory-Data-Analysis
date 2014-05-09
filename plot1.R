363a9d84cc3f3ea31597fc8ed712576f4f55c684
Adrian Cuyugan <adrian@foxyreign.com>
2014-05-09 14:08:58 -0500

    Use log on y?

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Load RDS
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Sample data for testing
+NEIsample <- NEI[sample(nrow(NEI), size=1000, replace=F), ]
+
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
+# for each of the years 1999, 2002, 2005, and 2008.
+barplot(tapply(X=NEI$Emissions, INDEX=NEI$year, FUN=sum), 
+        main=expression('Total Emission of PM'[2.5]), xlab='Year', ylab=expression('PM'[2.5]))
