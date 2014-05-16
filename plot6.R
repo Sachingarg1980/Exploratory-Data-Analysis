076e3d376eb4e1008e39e471475c14dc5b554a84
Adrian Cuyugan <adrian@foxyreign.com>
2014-05-16 08:41:51 +0800

    Final. Added text value labels in x-axis.

--- plot6.R
+++ plot6.R
@@ -4,8 +4,8 @@ NEI <- readRDS("summarySCC_PM25.rds")
 NEI$year <- factor(NEI$year, levels=c('1999', '2002', '2005', '2008'))
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# Baltimore City, Maryland == fips
-# Los Angeles, California == fips
+# Baltimore City, Maryland
+# Los Angeles County, California
 MD.onroad <- subset(NEI, fips == '24510' & type == 'ON-ROAD')
 CA.onroad <- subset(NEI, fips == '06037' & type == 'ON-ROAD')
 
@@ -26,5 +26,6 @@ DF <- as.data.frame(rbind(MD.DF, CA.DF))
 png('plot6.png')
 ggplot(data=DF, aes(x=year, y=Emissions)) + geom_bar(aes(fill=year)) + guides(fill=F) + 
     ggtitle('Total Emissions of Motor Vehicle Sources\nBaltimore City, Maryland vs. Los Angeles County, California') + 
-    ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + facet_grid(. ~ City)
+    ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position='none') + facet_grid(. ~ City) + 
+    geom_text(aes(label=round(Emissions,2), size=1, hjust=0.5, vjust=-1))
 dev.off()
