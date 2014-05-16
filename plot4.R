ff9862bf025b6f0920c334c242365d4cc44e32b7
Adrian Cuyugan <adrian@foxyreign.com>
2014-05-16 08:23:24 +0800

    Final. Changed y-axis to kilotons unit.

--- plot4.R
+++ plot4.R
@@ -15,10 +15,10 @@ colnames(merge.sum) <- c('Year', 'Emissions')
 # Across the United States, how have emissions from coal combustion-related sources 
 # changed from 1999-2008?
 png(filename='plot4.png')
-ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000000)) + 
+ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000)) + 
     geom_line(aes(group=1, col=Emissions)) + geom_point(aes(size=2, col=Emissions)) + 
     ggtitle(expression('Total Emissions of PM'[2.5])) + 
-    ylab(expression(paste('PM', ''[2.5], ' in megatons'))) + 
-    geom_text(aes(label=round(Emissions/1000000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
+    ylab(expression(paste('PM', ''[2.5], ' in kilotons'))) + 
+    geom_text(aes(label=round(Emissions/1000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
     theme(legend.position='none') + scale_colour_gradient(low='black', high='red')
 dev.off()
